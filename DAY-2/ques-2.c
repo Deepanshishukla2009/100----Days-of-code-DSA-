ce3a0c025d39e10b5ce81117cfd146410bb80bbe
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-02-05 08:53:04 +0530

    daily ques

--- DAY-2/ques-2.c
+++ DAY-2/ques-2.c
@@ -1,34 +1,44 @@
-#include <stdio.h>
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-int main() {
-    int n, pos;
-    printf("Enter number of elements (n): ");
-    scanf("%d", &n);
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
-    int arr[100]; 
-    printf("Enter %d integers:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104*/
+
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize <= 1) return 0;
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]; 
+        } else {
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit; 
+            }
+        }
     }
-
-    printf("Enter position to delete (1-based index): ");
-    scanf("%d", &pos);
-    
-    if (pos < 1 || pos > n) {
-        printf("Invalid position! Must be between 1 and %d.\n", n);
-        return 1;
-    }
-
-    for (int i = pos - 1; i < n - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-
-    n--;
-    printf("Array after deletion:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
+    return maxProfit;
 }
