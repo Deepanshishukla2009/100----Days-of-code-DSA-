0aee66c8575ce882a455def92a20bf5b0614abe6
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-04-20 21:51:37 +0530

    daily

---
+++ DAY-60/ques-2.c
@@ -0,0 +1,68 @@
+/*You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a node can monitor its parent, itself, and its immediate children.
+
+Return the minimum number of cameras needed to monitor all nodes of the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+Example 2:
+
+
+Input: root = [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+Node.val == 0
+ 
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int cameras = 0;
+
+// returns state: 0=not covered, 1=has camera, 2=covered
+int dfs(struct TreeNode* root) {
+    if (root == NULL)
+        return 2; // null nodes are considered covered
+
+    int left = dfs(root->left);
+    int right = dfs(root->right);
+
+    // if any child needs camera
+    if (left == 0 || right == 0) {
+        cameras++;
+        return 1; // place camera here
+    }
+
+    // if any child has camera
+    if (left == 1 || right == 1) {
+        return 2; // covered
+    }
+
+    // else not covered
+    return 0;
+}
+
+int minCameraCover(struct TreeNode* root) {
+    cameras = 0;
+
+    if (dfs(root) == 0)
+        cameras++;
+
+    return cameras;
+}
