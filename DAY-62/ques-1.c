05c5cc5d4d8260978b63354e75324dbc0ef34697
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-04-21 20:30:49 +0530

    daily

---
+++ DAY-62/ques-1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Array of adjacency lists
+    struct Node* adj[n];
+
+    // Initialize all lists as empty
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    // Read edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        // Add edge u -> v
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        // For undirected graph, also add v -> u
+        struct Node* newNode2 = createNode(u);
+        newNode2->next = adj[v];
+        adj[v] = newNode2;
+    }
+
+    // Print adjacency list
+    for (int i = 0; i < n; i++) {
+        printf("%d: ", i);
+        struct Node* temp = adj[i];
+        while (temp != NULL) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+
+    return 0;
+}
