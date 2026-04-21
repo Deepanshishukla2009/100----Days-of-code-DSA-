70fa42aad289bfe40e58c2d04f7369d0e7ce22e0
Deepanshishukla2009 <shuklashiri154@gmail.com>
2026-04-21 21:34:33 +0530

    daily

---
+++ DAY-65/ques-1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Create node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// DFS to detect cycle
+int dfs(int v, int parent, struct Node* adj[], int visited[]) {
+    visited[v] = 1;
+
+    struct Node* temp = adj[v];
+    while (temp != NULL) {
+        int neighbor = temp->vertex;
+
+        if (!visited[neighbor]) {
+            if (dfs(neighbor, v, adj, visited))
+                return 1;
+        }
+        else if (neighbor != parent) {
+            return 1; // cycle found
+        }
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n];
+
+    // Initialize
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        // Undirected graph
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        struct Node* newNode2 = createNode(u);
+        newNode2->next = adj[v];
+        adj[v] = newNode2;
+    }
+
+    int visited[n];
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    // Check all components
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(i, -1, adj, visited)) {
+                printf("YES\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("NO\n");
+    return 0;
+}
